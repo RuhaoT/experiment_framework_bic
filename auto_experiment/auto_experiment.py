0a4a704ea4c55e4c2bef4f808ba120ee5521d83a
Ruhao Tian <ruhaot2020@stu.xjtu.edu.cn>
2024-12-05 23:32:19 +0800

    Working on a distributed experiment executor.

--- auto_experiment/auto_experiment.py
+++ auto_experiment/auto_experiment.py
@@ -8,6 +8,7 @@ batch experiments.
 
 from abc import ABC, abstractmethod
 import tqdm
+import torch
 
 
 class AutoExperiment(ABC):
@@ -86,6 +87,55 @@ class SimpleBatchExperiment(AutoExperiment):
             for _ in range(self.batch):
                 self.experiment_interface.execute_experiment_process(parameters, self.dataset)
                 progress_bar.update(1)
+    
+    def evaluate(self):
+        """
+        Evaluate the experiment.
+        """
+        self.experiment_interface.summarize_results()
+        
+class CudaDistributedExperiment(AutoExperiment):
+    """
+    This is the class for CUDA distributed experiments.
+    """
+
+    def __init__(self, experiment_interface: ExperimentInterface, cuda: str | list[str] = "max") -> None:
+        """
+        This is the constructor of the class.
+        
+        Args:
+            experiment_interface (ExperimentInterface): The experiment interface.
+            cuda (str | list[str]): The CUDA device(s) to use. Use "max" to use
+                the maximum available CUDA device. Use "none" to use CPU. Use a
+                list of CUDA device IDs to specify the CUDA devices.
+        """
+        
+        # check and set the CUDA device(s)
+        self.devices = []
+        self.device_count = 0
+        if cuda == "none":
+            self.devices = ["cpu"]
+        elif cuda == "max":
+            # get device count
+            self.device_count = torch.cuda.device_count()
+            # check and append all available devices
+            if self.device_count == 0:
+                # raise error if no CUDA device is available
+                raise ValueError("No CUDA device is available.")
+            for i in range(self.device_count):
+                self.devices.append(f"cuda:{i}")
+        elif isinstance(cuda, list):
+            self.devices = [f"cuda:{i}" for i in cuda]
+        
+        self.experiment_interface = experiment_interface
+        self.parameter_group = self.experiment_interface.load_parameters()
+        self.dataset = self.experiment_interface.load_dataset()
+
+    def run(self):
+        """
+        Run the experiment on the CUDA device(s) in parallel.
+        """
+        # TODO(Ruhao Tian): Implement the CUDA distributed experiment.
     
     def evaluate(self):
         """
