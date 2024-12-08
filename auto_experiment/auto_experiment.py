ac07433b18d0bb2194a4828bbf73cda53a920489
Ruhao Tian <ruhaot2020@stu.xjtu.edu.cn>
2024-12-08 16:51:38 +0800

    Implemented CUDA-based distributed experiment executor.

--- auto_experiment/auto_experiment.py
+++ auto_experiment/auto_experiment.py
@@ -7,8 +7,13 @@ batch experiments.
 """
 
 from abc import ABC, abstractmethod
+import sys
+import os
+import signal
 import tqdm
 import torch
+import torch.multiprocessing as mp
+import termcolor
 
 
 class AutoExperiment(ABC):
@@ -130,12 +135,62 @@ class CudaDistributedExperiment(AutoExperiment):
         self.experiment_interface = experiment_interface
         self.parameter_group = self.experiment_interface.load_parameters()
         self.dataset = self.experiment_interface.load_dataset()
+        
+        # prepare for multiprocessing
+        # create a device Queue act as GPU pool
+        self.device_queue = mp.Manager().Queue()
+        for device in self.devices:
+            self.device_queue.put(device)
+        # create a process list
+        self.process_list = []
+    
+    def _sigint_handler(self, sig, frame):
+        """
+        Signal handler for SIGINT.
+        """
+        # close all processes
+        termcolor.cprint("SIGINT received, terminating processes...", "yellow", attrs=["bold"])
+        termcolor.cprint("Please wait. Force quitting may result in VRAM leakage.", "yellow", attrs=["bold"])
+        for process in self.process_list:
+            process.terminate()
+        sys.exit(0)
+
+    
+    def _map_experiment(self, parameters, dataset, device):
+        """
+        Map the experiment process.
+        """
+        with torch.cuda.device(device):
+            self.experiment_interface.execute_experiment_process(parameters, dataset)
+        # return the device to the device queue
+        self.device_queue.put(device)
 
     def run(self):
         """
         Run the experiment on the CUDA device(s) in parallel.
         """
         # TODO(Ruhao Tian): Implement the CUDA distributed experiment.
+        # set the signal handler
+        signal.signal(signal.SIGINT, self._sigint_handler)
+        
+        # create a progress bar
+        progress_bar = tqdm.tqdm(total=len(self.parameter_group))
+        
+        # iterate over the parameter group
+        for parameters in self.parameter_group:
+            # get the device from the device queue
+            device = self.device_queue.get()
+            # execute the experiment process
+            new_process = mp.Process(target=self._map_experiment, args=(parameters, self.dataset, device))
+            new_process.start()
+            self.process_list.append(new_process)
+            # update the progress bar
+            progress_bar.update(1)
+        
+        # wait for all processes to finish
+        for process in self.process_list:
+            process.join()
+            
     
     def evaluate(self):
         """
